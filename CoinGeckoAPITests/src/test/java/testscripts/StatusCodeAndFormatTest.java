a7ff7242ac41c8beeb20cf059ae5eced3b5a3d3f
ashwinis29 <140146016+ashwinis29@users.noreply.github.com>
2025-03-02 20:16:17 +0530

    Update StatusCodeAndFormatTest.java

--- CoinGeckoAPITests/src/test/java/testscripts/StatusCodeAndFormatTest.java
+++ CoinGeckoAPITests/src/test/java/testscripts/StatusCodeAndFormatTest.java
@@ -12,7 +12,7 @@ public class StatusCodeAndFormatTest extends BaseClass
 	@Test(description = "TC_001: Validate Status Code", priority = 1)
 	public void verifyAPIStatusCode()
 	{
-		Response response = APITestSteps.getResponseForCurrency(javaUtils.propertyData("endpoint"), "usd");
+	    Response response = APITestSteps.getResponseForCurrency(javaUtils.propertyData("endpoint"), "usd");
 
 	    Assert.assertEquals(response.getStatusCode(), 200, "Expected Status code: 200 but got "+response.getStatusCode());
 	    test.pass(response.getStatusCode() == 200 ? "Status code is 200" : "Status code is not 200");
@@ -22,7 +22,7 @@ public class StatusCodeAndFormatTest extends BaseClass
 	@Test(description = "TC_002: Validate Response Format", priority = 2)
 	public void verifyAPIResponseFormat()
 	{
-		Response response = APITestSteps.getResponseForCurrency(javaUtils.propertyData("endpoint"), "usd");
+	    Response response = APITestSteps.getResponseForCurrency(javaUtils.propertyData("endpoint"), "usd");
 		
 	    Assert.assertTrue(response.getContentType().equals("application/json; charset=utf-8"), "Response is not in JSON format");
 	    test.pass(response.getContentType().equals("application/json; charset=utf-8") ? "Response is in JSON format" : "Response is not in Json Format");
